bc3286034e0ff5e09745f96a7f572268e04ef53e
dmstjd0214@naver.com <dmstjd0214@naver.com>
2024-03-18 08:38:23 +0900

    도메인 주도 개발 시작 책 프로젝트 구성

---
+++ dodrde/src/main/java/learn/dodrde/DodrdeApplication.java
@@ -0,0 +1,13 @@
+package learn.dodrde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodrdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodrdeApplication.class, args);
+	}
+
+}
---
+++ dodrde/src/test/java/learn/dodrde/DodrdeApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.dodrde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodrdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
